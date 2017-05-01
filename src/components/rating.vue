9f2f7f1ef6daad65906e9704b7a351002d463e1e
Nicholas Achilles <s4317811@student.uq.edu.au>
2017-05-01 11:16:13 +1000

    Fixed clicking the icon or the button for the rating

--- src/components/rating.vue
+++ src/components/rating.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<md-button v-for="i in +max" :key="i" class="md-icon-button">
-			<md-icon ref="icons" @click.native="changeRating" v-bind:class="{'md-primary': (i-1) <= selectedIndex}">star</md-icon>
+		<md-button v-for="i in +max" :key="i" class="md-icon-button" @click.native="changeRating" ref="icons">
+			<md-icon v-bind:class="{'md-primary': (i-1) <= selectedIndex}">star</md-icon>
 		</md-button>
 	</div>
 </template>
@@ -18,8 +18,14 @@
 		selectedIndex = -1;
 
 		changeRating(e: Event) {
+			// Check if the user clicked on child. If so, select the parent button
+			let target = e.target as HTMLElement;
+			if (target.tagName == "I") {
+				target = target.parentElement;
+			}
+			
 			const icons = this.$refs["icons"] as Array<Vue>;
-			this.selectedIndex = icons.findIndex((x: Vue) => x.$el == e.target);
+			this.selectedIndex = icons.findIndex((x: Vue) => x.$el == target);
 		}
 	}
 </script>
